f5a192054c5d4dfef04ba0e8d6de22c3e69a33ba
akash <xatriya>
2021-04-24 21:24:43 +0530

    refactored

--- part1/app1/src/App.js
+++ part1/app1/src/App.js
@@ -6,13 +6,22 @@ const Header = (props) => (
   </div>
 );
 
-const Content = (props) => (
-  <div id="content">
-    <Part part = {props.part1} exercises = {props.exercises1}/>
-    <Part part = {props.part2} exercises = {props.exercises2}/>
-    <Part part = {props.part3} exercises = {props.exercises3}/>
-  </div>
-);
+const Content = (props) => {
+  // using loop to avoid duplicate code
+  // saving jsx in items array and returning it as whole
+  const items = []
+  props.parts.forEach((element, index )=> {
+   items.push(
+    <Part part = {element} exercises = {props.exercises[index]}/>
+    ) 
+  });  
+
+  return (
+    <div id="content">
+      {items}
+    </div>
+  );
+}
 
 const Total = (props) => (
   <div id="Total">
@@ -29,17 +38,21 @@ const Part = (props) => (
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
   const part3 = 'State of a component'
+  const exercises1 = 10
+  const exercises2 = 7
   const exercises3 = 14
 
+  const parts = [part1,part2,part3];
+  const exercises = [exercises1,exercises2,exercises3];
+
+  // Total uses reduce function to calculate the sum
   return (
     <div>
       <Header course = {course}/>
-      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
-      <Total total = {exercises1 + exercises2 + exercises3}/>
+      <Content parts = {parts} exercises = {exercises}/>
+      <Total total = {exercises.reduce((a,b) => a + b, 0)}/>
     </div>
   )
 }
